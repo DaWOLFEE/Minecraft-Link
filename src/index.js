f9b98ca70d29be875e46822387ac028bd818dacd
DaWOLFE <46287894+DaWOLFEE@users.noreply.github.com>
2022-12-27 00:40:56 -0700

    Endpoint & Refresh
    - Added first endpoint for api, basics and barebones
    - Fixed an issue with new authentications trying to insert into a non-existant column
    - Completed the token hourly check for expired tokens

--- src/index.js
+++ src/index.js
@@ -39,8 +39,8 @@ app.get('/api/auth/discord/redirect', async ({ query }, response) => {
             let user = await userResult.body.json();
 
             let oauth = new Database('Dev_Link');
-            let authed = await oauth.query(`SELECT DiscordID FROM Tokens WHERE DiscordID = ${user.id}`);
-            authed ? await oauth.query(`UPDATE Tokens SET Auth = '${oauthData.access_token}', Refresh = '${oauthData.refresh_token}', Expires = ${Date.now() + (oauthData.expires_in * 1000)} WHERE DiscordID = ${user.id}`) : await oauth.query(`INSERT INTO Tokens (DiscordID, Auth, Refresh, Timestamp) VALUES (${user.id}, '${oauthData.access_token}', '${oauthData.refresh_token}', ${Date.now() + (oauthData.expires_in * 1000)})`);
+            let authed = await oauth.query(`SELECT DiscordID FROM Tokens WHERE DiscordID = '${user.id}'`);
+            authed ? await oauth.query(`UPDATE Tokens SET Auth = '${oauthData.access_token}', Refresh = '${oauthData.refresh_token}', Expires = ${Date.now() + (oauthData.expires_in * 1000)} WHERE DiscordID = '${user.id}'`) : await oauth.query(`INSERT INTO Tokens (DiscordID, Auth, Refresh, Expired) VALUES ('${user.id}', '${oauthData.access_token}', '${oauthData.refresh_token}', ${Date.now() + (oauthData.expires_in * 1000)})`);
             await oauth.close();
 
 			console.log(user);
@@ -54,8 +54,46 @@ app.get('/api/auth/discord/redirect', async ({ query }, response) => {
 	return response.redirect('https://discord.com/oauth2/authorized');
 });
 
-app.get('/api/user/?id', (req, res) => {
-    console.log(req);
+app.get('/api/user/:id', async (req, res) => {
+    let { id } = req.params;
+
+    let reg = new RegExp(/^\d+$/g);
+    if (!reg.test(id)) return res.status(401).send(`Invalid Parameter: /api/user/(id) must be numerical.`);
+
+    let oauth = new Database('Dev_Link');
+    let token = await oauth.query(`SELECT * FROM Tokens WHERE DiscordID = '${id}'`);
+
+    if (!token) return res.status(404).send(`Non-existant Parameter: /apu/user/${id} hasn't been linked yet.`);
+    if (token.Expires < Date.now()) {
+        const tokenResponseData = await request('https://discord.com/api/oauth2/token', {
+				method: 'POST',
+				body: new URLSearchParams({
+					client_id: clientId,
+					client_secret: clientSecret,
+					refresh_token: Refresh,
+					grant_type: 'refresh_token',
+				}).toString(),
+				headers: {
+					'Content-Type': 'application/x-www-form-urlencoded',
+				},
+			});
+
+			const oauthData = await tokenResponseData.body.json();
+
+            await oauth.query(`UPDATE Tokens SET Auth = '${oauthData.access_token}', Refresh = '${oauthData.refresh_token}', Expires = ${Date.now() + (oauthData.expires_in * 1000)} WHERE DiscordID = '${DiscordID}'`);
+            token.Auth = oauthData.access_token;
+    }
+
+    await oauth.close();
+
+    const userResult = await request('https://discord.com/api/users/@me/connections', {
+        headers: {
+            authorization: `Bearer ${token.Auth}`,
+        },
+    });
+
+    let user = await userResult.body.json();
+    res.status(200).send(user);
 });
 
 app.listen(port, () => console.log(`${port}`));
--- src/refresh.js
+++ src/refresh.js
@@ -1,5 +1,7 @@
 const { CronJob } = require('cron');
 const Database = require('./resources/Database.js');
+const { request } = require('undici');
+const { clientId, clientSecret } = require('./config.json');
 
 let iterator = new CronJob('0 0 * * * *', async () => {
     let oauth = new Database('Dev_Link');
@@ -10,7 +12,24 @@ let iterator = new CronJob('0 0 * * * *', async () => {
         for (let { DiscordID, Refresh } of expired) {
             console.log(DiscordID, Refresh);
 
-            //Hourly refresh of expired tokens
+            const tokenResponseData = await request('https://discord.com/api/oauth2/token', {
+				method: 'POST',
+				body: new URLSearchParams({
+					client_id: clientId,
+					client_secret: clientSecret,
+					refresh_token: Refresh,
+					grant_type: 'refresh_token',
+				}).toString(),
+				headers: {
+					'Content-Type': 'application/x-www-form-urlencoded',
+				},
+			});
+
+			const oauthData = await tokenResponseData.body.json();
+
+            let oauth = new Database('Dev_Link');
+            await oauth.query(`UPDATE Tokens SET Auth = '${oauthData.access_token}', Refresh = '${oauthData.refresh_token}', Expires = ${Date.now() + (oauthData.expires_in * 1000)} WHERE DiscordID = '${DiscordID}'`);
+            await oauth.close();
             //Remember to check response headers from Discord for rate limits
         }
     }
